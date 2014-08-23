b0b07bb4637f91f09335113f1c65c1d95cc0c541
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2014-08-23 10:34:45 -0700

    Added sound effect events and listeners for planet drop sound effect

--- src/audio.go
+++ src/audio.go
@@ -3,31 +3,46 @@ package main
 import twodee "../libs/twodee"
 
 type AudioSystem struct {
-	app                       *Application
-	backgroundMusic           *twodee.Music
-	backgroundMusicObserverId int
+	app                        *Application
+	backgroundMusic            *twodee.Music
+	planetDropEffect           *twodee.SoundEffect
+	backgroundMusicObserverId  int
+	planetDropEffectObserverId int
 }
 
 func (a *AudioSystem) PlayBackgroundMusic(e twodee.GETyper) {
 	a.backgroundMusic.Play(-1)
 }
 
+func (a *AudioSystem) PlayPlanetDropEffect(e twodee.GETyper) {
+	a.planetDropEffect.PlayChannel(2, 1)
+}
+
 func (a *AudioSystem) Delete() {
 	a.app.GameEventHandler.RemoveObserver(PlayBackgroundMusic, a.backgroundMusicObserverId)
+	a.app.GameEventHandler.RemoveObserver(PlayPlanetDropEffect, a.planetDropEffectObserverId)
 	a.backgroundMusic.Delete()
+	a.planetDropEffect.Delete()
 }
 
 func NewAudioSystem(app *Application) (audioSystem *AudioSystem, err error) {
 	var (
-		backgroundMusic *twodee.Music
+		backgroundMusic  *twodee.Music
+		planetDropEffect *twodee.SoundEffect
 	)
 	if backgroundMusic, err = twodee.NewMusic("assets/music/BGM1.ogg"); err != nil {
 		return
 	}
-	audioSystem = &AudioSystem{
-		app:             app,
-		backgroundMusic: backgroundMusic,
+	if planetDropEffect, err = twodee.NewSoundEffect("assets/sound_effects/PlanetDrop.ogg"); err != nil {
+		return
 	}
+	audioSystem = &AudioSystem{
+		app:              app,
+		backgroundMusic:  backgroundMusic,
+		planetDropEffect: planetDropEffect,
+	}
+	planetDropEffect.SetVolume(40)
 	audioSystem.backgroundMusicObserverId = app.GameEventHandler.AddObserver(PlayBackgroundMusic, audioSystem.PlayBackgroundMusic)
+	audioSystem.planetDropEffectObserverId = app.GameEventHandler.AddObserver(PlayPlanetDropEffect, audioSystem.PlayPlanetDropEffect)
 	return
 }
